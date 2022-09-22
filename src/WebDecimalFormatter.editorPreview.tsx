a2cd4f8d66004c950a3c794350ea5535210255fc
Alexandre Sepe <alexandre.sepe@truechange.com.br>
2022-09-22 17:58:00 -0300

    impl. mudanças no webDecimalFormatter

--- src/WebDecimalFormatter.editorPreview.tsx
+++ src/WebDecimalFormatter.editorPreview.tsx
@@ -9,24 +9,24 @@ import { money } from "./Money";
 export class preview extends Component<WebDecimalFormatterPreviewProps> {
     render(): ReactNode {
         return (
-            <div ref={this.parentInline}>
-                <BadgeSample {...this.transformProps(this.props)}></BadgeSample>
-            </div>
+            // <div ref={this.parentInline}>
+            <BadgeSample {...this.transformProps(this.props)}></BadgeSample>
+            // </div>
         );
     }
 
-    private parentInline(node?: HTMLElement | null): void {
-        // Temporary fix, the web modeler add a containing div, to render inline we need to change it.
-        if (node && node.parentElement && node.parentElement.parentElement) {
-            node.parentElement.parentElement.style.display = "inline-block";
-        }
-    }
+    // private parentInline(node?: HTMLElement | null): void {
+    //     // Temporary fix, the web modeler add a containing div, to render inline we need to change it.
+    //     if (node && node.parentElement && node.parentElement.parentElement) {
+    //         node.parentElement.parentElement.style.display = "inline-block";
+    //     }
+    // }
 
     private transformProps(props: WebDecimalFormatterPreviewProps): BadgeSampleProps {
         const decimalValue = money(
             123456.789,
-            props.decimalSeparator,
-            props.thousandsSeparator,
+            props.decimalSeparator.replaceAll("'", ""),
+            props.thousandsSeparator.replaceAll("'", ""),
             props.decimalCount || 2
         );
 
